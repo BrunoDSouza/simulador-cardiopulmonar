e81b0009601ce54db61c21333da97889df973ae1
Bruno D. Souza <brunosouzasantos07@gmail.com>
2017-10-12 20:59:48 -0300

    setup structure folders and modules

---
+++ src/app/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style src="@/assets/sass/app.scss" lang="sass"></style>
